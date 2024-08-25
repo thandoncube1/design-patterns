e1aeb35a49c23e17c215411215dfa02fea7adccb
thandoncube1 <ncubethando20@gmail.com>
2024-08-25 03:53:19 +0000

    Working on the client code for the builder pattern & notes

--- builder.cpp
+++ builder.cpp
@@ -120,7 +120,7 @@ class Director {
      * building steps.
      */
 
-    void BuilMinimalViableProduct() {
+    void BuildMinimalViableProduct() {
         this->builder->ProducePartA();
     }
 
@@ -131,3 +131,39 @@ class Director {
     }
 };
 
+/**
+ * The client code creates a builder object, passes it to the director and then 
+ * initiates the construction process. The end result is retrieved from the
+ * builder object.
+ */
+
+/**
+ * I used raw pointers for simplicity however you may prefer to use smart pointers here.
+ */
+void ClientCode(Director& director) {
+    ConcreteBuilder1* builder = new ConcreteBuilder1();
+    director.set_builder(builder);
+    std::cout << "Standard basic product:\n";
+    director.BuildMinimalViableProduct();
+
+    Product1* p = builder->GetProduct();
+    p->ListParts();
+    delete p;
+
+    std::cout << "Standard full featured product:\n";
+    director.BuildFullFeaturedProduct();
+
+    p = builder->GetProduct();
+    p->ListParts();
+    delete p;
+
+    // Remember, the Builder pattern can be used without a Director class.
+    std::cout << "Custom product:\n";
+    builder->ProducePartA();
+    builder->ProducePartC();
+    p = builder->GetProduct();
+    p->ListParts();
+    delete p;
+
+    delete builder;
+}
