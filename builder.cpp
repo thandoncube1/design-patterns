739775a2e9aaf1ee5d6f3bb52212520a693460b9
thandoncube1 <ncubethando20@gmail.com>
2024-08-25 03:42:54 +0000

    Working on the Director code for the Builder pattern

--- builder.cpp
+++ builder.cpp
@@ -92,3 +92,42 @@ class ConcreteBuilder1: public Builder {
         return result;
     }
 };
+
+/**
+ * The Director is only responsible for executing the building steps in a 
+ * particular sequence. It is helpful when producing products according to a 
+ * specific order or configuration. Strictly speaking, the Director class is optional, since the client can control builders directly.
+ */
+
+class Director {
+    /**
+     * @var Builder
+     */
+    private:
+    Builder* builder;
+    /**
+     * The Director works with any builder instance that the client code passes
+     * to it. This way, the client code may alter the final type of the newly assembled product.
+     */
+    public:
+
+    void set_builder(Builder* builder) {
+        this->builder = builder;
+    }
+
+    /**
+     * The Director can construct several product variations using the same
+     * building steps.
+     */
+
+    void BuilMinimalViableProduct() {
+        this->builder->ProducePartA();
+    }
+
+    void BuildFullFeaturedProduct() {
+        this->builder->ProducePartA();
+        this->builder->ProducePartB();
+        this->builder->ProducePartC();
+    }
+};
+
