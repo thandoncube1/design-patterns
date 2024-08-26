4f241c931e83541fae37fb8584678cb5b6df3888
thandoncube1 <ncubethando20@gmail.com>
2024-08-26 08:59:53 +0000

    Working with functional pipelines for Java

--- Iterators.java
+++ Iterators.java
@@ -1,10 +1,7 @@
 import java.util.*;
 
 public class Iterators {
-    public static void main(String[] args) {
-        var names = List.of("Thando", "Eric", "Wilson", "Bruce", "Nemo", "Darla", "Marlin", "Jacques");
-        // External iterators - We control the iteration
-
+    public static void iterator(List<String> names) {
         // for (int i = 0; i <= names.size(); ...) {} // Verbose
         int count = 0;
         for (var name: names) {
@@ -30,4 +27,43 @@ public class Iterators {
             // Limit and takeWhile are thefunction equivalent of the break
             // from the imperative style.
     }
+    public static void main(String[] args) {
+        var names = List.of("Thando", "Eric", "Wilson", "Bruce", "Nemo", "Darla", "Marlin", "Jacques");
+        // External iterators - We control the iteration
+
+        System.out.println("------------");
+
+        iterator(names);
+
+        System.out.println("--------------");
+
+        var result1 = new ArrayList<String>();
+
+        for (var name: names) {
+            if (name.length() == 4) {
+                result1.add(name.toUpperCase());
+            }
+        }
+
+        System.out.println(result1);
+
+        var result2 = new ArrayList<String>();
+        names.stream()
+            .filter(name -> name.length() == 4)
+            .map(String::toUpperCase)
+            .forEach(name -> result2.add(name)); // BAD IDEA - Modifying a pipeline that is given.
+            // The functional pipeline is *not* pure
+            // We are doing shared mutability - Changing to .parallel() or
+            // by changing .stream() to .parallelStream()
+
+            System.out.println(result2);
+
+            // Functional pipeline offers internal iterators
+            // is less complex
+            // easy to modify
+            // easy to understand
+            // but...
+    }
 }
+
+// It is very important that we make the functional pipeline as pure as possible. Avoid shared mutable variables.
