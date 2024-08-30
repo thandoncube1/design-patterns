5ffb75237c4d43920778172653e0549b8fb675de
thandoncube1 <ncubethando20@gmail.com>
2024-08-30 07:13:40 +0000

    Working with pipelines for resources.

--- Interfaces/NewSample.java
+++ Interfaces/NewSample.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.function.Consumer;
 
 class Resource implements AutoCloseable {
     public Resource() { System.out.println("created..."); }
@@ -10,8 +11,19 @@ class Resource implements AutoCloseable {
         System.out.println("opt2...");
         return this;
     }
+
     public void close() {
-        System.out.println("Release external resource");
+        System.out.println("Close resource.....");
+    }
+
+    public static void use(Consumer<Resource> block) {
+        Resource resource = new Resource();
+
+        try {
+            block.accept(resource);
+        } finally {
+            resource.close(); // after
+        }
     }
 }
 
@@ -19,11 +31,16 @@ public class NewSample {
     public static void main(String[] args) {
         // ARM - Automatic Resource Management
         // We can use a try with resources
-        try (Resource resource = new Resource()){
-            resource.opt1();
-            resource.opt2();
-        }
+        // try (Resource resource = new Resource()){
+        //     resource.opt1();
+        //     resource.opt2();
+        // }
         // In the case of an exception
+
+        Resource.use((resource) -> {
+            resource.opt1()
+                .opt2();
+        });
         System.out.println("Easy to forget and verbose.");
     }
 }
